cf23c13aa6b323edd921f23398d45c37ab8fa648
Jeff3181 <jeff3181@users.noreply.github.com>
2018-01-01 19:38:57 +0100

    adding hostlistener et hostbinding exemples

--- directivescustom/src/app/shared/directives/color.directive.ts
+++ directivescustom/src/app/shared/directives/color.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
 	selector:'[appColor]'
@@ -8,8 +8,20 @@ export class ColorDirective implements OnInit{
 
 	}
 	ngOnInit(){
-		this.el.nativeElement.style.color = 'red';
-		this.renderer.setStyle(this.el.nativeElement, 'color', 'red');
+		//this.el.nativeElement.style.color = 'red';
+		//this.renderer.setStyle(this.el.nativeElement, 'color', 'red');
+
 
 	}
+
+	@HostListener('click') click(){
+
+		//this.renderer.setStyle(this.el.nativeElement, 'background-color', 'black');
+		this.backgroundColor = 'black';
+	}
+
+	@HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+	@HostBinding('style.color') color = 'blue';
+
+	//other exemple https://plnkr.co/edit/jOeLeysHOgEBonhjS4x2?p=info
 }
