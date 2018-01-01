2cebd1ab665afe67cbfb892d35a5b5e6ff3a43b2
Jeff3181 <jeff3181@users.noreply.github.com>
2018-01-01 18:51:40 +0100

    creating custom directives exemple

---
+++ directivescustom/src/app/shared/directives/color.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+
+@Directive({
+	selector:'[appColor]'
+})
+export class ColorDirective implements OnInit{
+	constructor(private el: ElementRef, private renderer: Renderer2){
+
+	}
+	ngOnInit(){
+		this.el.nativeElement.style.color = 'red';
+		this.renderer.setStyle(this.el.nativeElement, 'color', 'red');
+
+	}
+}
