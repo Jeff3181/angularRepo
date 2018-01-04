dfd17d31082d7d6b9f2b1be6253645e29ba27f11
Jeff3181 <JeanFranois.CHARNAY@cegedim.com>
2018-01-04 13:20:27 +0100

    adding plunker links

--- exemplecompoment/src/app/item/item.component.ts
+++ exemplecompoment/src/app/item/item.component.ts
@@ -17,4 +17,15 @@ export class ItemComponent implements OnInit {
   		//this.deleteF.emit(this.fruit);
   		console.log(this.el);
   	}
+
+    // communication entre composants https://plnkr.co/edit/qtvPLSNriWrGQhd7O9Z9?p=info
+    // Input https://plnkr.co/edit/qtvPLSNriWrGQhd7O9Z9?p=info
+    //Input https://plnkr.co/edit/UlqfkUw3yQFROUrkNVQF
+    //output https://plnkr.co/edit/P706FjEaiU3R6kTOggCP
+    //Output alias https://plnkr.co/edit/o8Cpe3A11WpdxDoULXlH
+    //local variable https://plnkr.co/edit/vYTKfLdaa8oRjWw80vcr
+    //viewChild https://plnkr.co/edit/aZVShOJPSDrEH8SfXu6u
+    //ngContent https://plnkr.co/edit/SNEro7or8h0wBrVeFWfU
+    //ngContent https://plnkr.co/edit/aY8G4sbaE7PRZ05azXhq
+    //contentChild https://plnkr.co/edit/aY8G4sbaE7PRZ05azXhq
 }
