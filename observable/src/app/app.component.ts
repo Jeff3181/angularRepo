1f9705051aef793595542e075f98f4e348a28e73
Jeff3181 <JeanFranois.CHARNAY@cegedim.com>
2018-01-08 16:05:54 +0100

    Adding observable project

---
+++ observable/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit} from '@angular/core';
+import { Observable, Observer, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+
+  ngOnInit(){
+
+  	const observer: Observer<any> = {
+	next: x => console.log('Observer a reçu une valeur : ' + x),
+	error: err => console.log('Observer a reçu une erreur : ' + err),
+	complete: () => console.log('Observer a reçu une notification de complétion')
+  	};
+
+  	const obs = Observable.create(observer =>{
+  		observer.next(1);
+  		observer.next(2);
+  		setTimeout(function(){
+  			observer.next(42);
+  			observer.complete();
+  			observer.next(42);
+  		}, 2000);
+  	})
+
+  	const sub: Subscription = obs.subscribe( x => {
+  		console.log(x);
+  	})
+  	obs.subscribe(observer);
+  }
+
+}
