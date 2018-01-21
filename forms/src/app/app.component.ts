4bd1db23aa059dbd50b60e892aba46705e2be864
Jeff3181 <jeff3181@users.noreply.github.com>
2018-01-21 21:29:02 +0100

    using FormGroug and FormControl

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -14,12 +14,25 @@ export class AppComponent implements OnInit{
   		username: new FormControl(''),
   		email: new FormControl(''),
   		password: new FormControl(''),
-
   	})
+  	console.log(this.myForm);
+  	//SetValue tous les champs doivent être complétés
+  	this.myForm.setValue({
+  		username: 'jean',
+  		email: 'jean@live.fr',
+  		password: '1234'
+  	})
+
+  	//PacthValue ne set que les champs renseignés
+  	this.myForm.patchValue({
+  		username: 'Paul'
+	})
   }
   submit(){
   	console.log(this.myForm);
+  	this.myForm.reset();
   }
   //https://plnkr.co/edit/RxfBHLnIhRsezvSKpGO1?p=info
   //https://plnkr.co/edit/hnVN3iFh1v7Ue7EcliID?p=info
+  //https://plnkr.co/edit/ptz7YJNJocmtitBQl9Xh?p=info
 }
