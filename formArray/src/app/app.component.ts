76c2ac52cd5b174345ca025d966d7c6afb957c02
Jeff3181 <JeanFranois.CHARNAY@cegedim.com>
2018-01-23 14:47:25 +0100

    Forms + formArray + angular-tour-of-heroes

--- formArray/src/app/app.component.ts
+++ formArray/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators} from '@angular/forms';
 import { Observable} from 'rxjs';
 
 @Component({
@@ -16,13 +16,11 @@ export class AppComponent implements OnInit{
 
   ngOnInit(){
   	this.myForm = new FormGroup({
-      login: new FormGroup({
-        username: new FormControl(''),
-        email: new FormControl('')
-      }),
-  		//username: new FormControl('', [Validators.required, this.validator1], this.validatorAsync),
-  		//email: new FormControl(''),
-  		password: new FormControl('', this.noMatchingPassword.bind(this)),
+     
+  		username: new FormControl(''),
+  	  email: new FormControl(''),
+      hobbies: new FormArray([]),
+  		password: new FormControl(''),
   	})
   	console.log(this.myForm);
   	//SetValue tous les champs doivent être complétés
@@ -38,28 +36,8 @@ export class AppComponent implements OnInit{
 	})*/
   }
 
-    noMatchingPassword(formControl: FormControl): {[s: string]: boolean}{
-      if(formControl.value != this.confirm.nativeElement.value){
-        return {noMatch : true}
-      }else{
-        return null;
-      }
-  }
-
-  validator1(formControl: FormControl): {[s: string]: boolean}{
-    if (formControl.value === 'paul'){
-      return { isPaul: true}
-    }else{
-      return null;
-    }
-  }
-
-  validatorAsync(formControl: FormControl): Promise<any> | Observable<any>{
-    return new Promise( (resolve, reject) => {
-      setTimeout( () =>{
-        resolve(null);
-      }, 30000)
-    })
+  addHobby(): void{
+    (<FormArray>this.myForm.get('hobbies')).controls.push(new FormControl(''))
   }
 
   submit(){
@@ -73,4 +51,5 @@ export class AppComponent implements OnInit{
   //https://plnkr.co/edit/DJAwV13fmeW8knsswFA9?p=info
   //https://plnkr.co/edit/KeMUgUEYpUtZtB2ZTwGP?p=info
   //https://plnkr.co/edit/S7L8Mb0MLpxuU6J7ceiW?p=preview
+  //https://plnkr.co/edit/4VfbUFOpK3LMUHHOoNX6?p=preview
 }
