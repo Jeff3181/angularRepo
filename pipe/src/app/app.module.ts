558f7a564068cb21962bd5bb8210a3a4fb7be6d7
jcharnay <JeanFranois.CHARNAY@cegedim.com>
2018-01-30 18:34:40 +0100

    completing pure pipes

--- pipe/src/app/app.module.ts
+++ pipe/src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CbPipe } from './cb.pipe';
+import { FormsModule} from '@angular/forms';
+
 
 
 
 import { AppComponent } from './app.component';
 import { PipepursetimpursComponent } from './pipepursetimpurs/pipepursetimpurs.component';
+import { FilterPipe } from './filter.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CbPipe,
-    PipepursetimpursComponent
+    PipepursetimpursComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
